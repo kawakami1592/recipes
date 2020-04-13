f15bccb06bca3db8dfaff125486755f03fff8d7e
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-04-13 18:18:25 +0900

    Initial commit

---
+++ db/migrate/20200412164231_create_recipes.rb
@@ -0,0 +1,13 @@
+class CreateRecipes < ActiveRecord::Migration[5.2]
+  def change
+    create_table :recipes do |t|
+      t.string     :title
+      t.references :user
+      t.references :category
+      t.text       :text
+      t.string     :image
+
+      t.timestamps
+    end
+  end
+end
