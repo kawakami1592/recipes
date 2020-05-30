cecf8863e546ded9c88ef92ee418205e664711cd
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-05-30 22:47:23 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,10 +45,11 @@ before :starting, 'deploy:upload'
 after :finishing, 'deploy:cleanup'
 end
 
-# # 必要に応じて/環境変数をcapistranoでの自動デプロイで利用
-# set :default_env, {
-#  rbenv_root: "/usr/local/rbenv",
-#  path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+# 必要に応じて/環境変数をcapistranoでの自動デプロイで利用
+set :default_env, {
+ rbenv_root: "/usr/local/rbenv",
+ path: "/usr/local/rbenv/shims:/usr/local/rbenv/bin:$PATH",
+ RAILS_MASTER_KEY: ENV['RAILS_MASTER_KEY'],
 #  AWS_ACCESS_KEY_ID: ENV["AWS_ACCESS_KEY_ID"],
 #  AWS_SECRET_ACCESS_KEY: ENV["AWS_SECRET_ACCESS_KEY"]
-# }
+}
