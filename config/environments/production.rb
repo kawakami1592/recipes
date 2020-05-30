ae82b333377c957778fa7b07e140cff86cb20d73
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-05-30 22:26:56 +0900

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,7 +12,7 @@ Rails.application.configure do
 
   # Full error reports are disabled and caching is turned on.
   # cofig.consider_all_requests_local       = false
-  ?config.consider_all_requests_local       = true
+  config.consider_all_requests_local       = true
 
   config.action_controller.perform_caching = true
 
