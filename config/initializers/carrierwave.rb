8a012d769f2c1d3457920edd0dc44f1e6eb119bd
kawakami1592 <57590363+kawakami1592@users.noreply.github.com>
2020-05-31 20:22:59 +0900

    S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -31,7 +31,7 @@ CarrierWave.configure do |config|
     config.fog_directory  = 'makotoresipesimage'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/makotoresipesimage'
   else
-    config.storage :file # 開発環境:public/uploades下に保存
+    config.storage = :file # 開発環境:public/uploades下に保存
     config.enable_processing = false if Rails.env.test? #test:処理をスキップ
   end  
 end
